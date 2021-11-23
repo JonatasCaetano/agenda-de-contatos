bec7b34b76c874e45305d772116ee361db8fd9d1
Jonatas Caetano <jonatas.calves@gmail.com>
2021-11-23 14:55:03 -0300

    Find by id method implemented

--- agenda-api/src/main/java/com/jonatas/agenda/controllers/ContactController.java
+++ agenda-api/src/main/java/com/jonatas/agenda/controllers/ContactController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,16 @@ public class ContactController {
 	public ResponseEntity<List<Contact>> findAll(){
 		List<Contact> contacts = contactRepository.findAll();
 		return ResponseEntity.ok().body(contacts);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Contact> findById(@PathVariable Long id){
+		try {
+			Contact contact = contactRepository.findById(id).get();
+			return ResponseEntity.ok().body(contact);
+		}catch(RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 }
