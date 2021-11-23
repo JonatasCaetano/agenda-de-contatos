240ef89266c970c3ecdb3cc1f224aa83d7dc5b1d
Jonatas Caetano <jonatas.calves@gmail.com>
2021-11-23 17:34:57 -0300

    Delete method implemented

--- agenda-api/src/main/java/com/jonatas/agenda/controllers/ContactController.java
+++ agenda-api/src/main/java/com/jonatas/agenda/controllers/ContactController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,16 @@ public class ContactController {
 			obj.setPhone(contact.getPhone());
 			contactRepository.save(obj);
 			return ResponseEntity.accepted().build();
+		}catch(RuntimeException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> deleteContact(@PathVariable Long id){
+		try {
+			contactRepository.deleteById(id);
+			return ResponseEntity.ok().build();
 		}catch(RuntimeException e) {
 			return ResponseEntity.notFound().build();
 		}
