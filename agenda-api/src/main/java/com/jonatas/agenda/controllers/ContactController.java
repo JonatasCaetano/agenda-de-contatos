e0c0b26617e4559d9eb95b362fbbb38a02c12cff
Jonatas Caetano <jonatas.calves@gmail.com>
2021-11-23 15:27:33 -0300

    Post method implemented

--- agenda-api/src/main/java/com/jonatas/agenda/controllers/ContactController.java
+++ agenda-api/src/main/java/com/jonatas/agenda/controllers/ContactController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +37,13 @@ public class ContactController {
 		}
 	}
 	
+	@PostMapping
+	public ResponseEntity<Void> saveContact(@RequestBody Contact contact){
+		try {
+			contactRepository.save(contact);
+			return ResponseEntity.created(null).build();
+		}catch(RuntimeException e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
 }
--- agenda-api/src/main/java/com/jonatas/agenda/entities/Contact.java
+++ agenda-api/src/main/java/com/jonatas/agenda/entities/Contact.java
@@ -1,5 +1,6 @@
 package com.jonatas.agenda.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,7 +12,9 @@ public class Contact {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false)
 	private String name;
+	@Column(nullable = false)
 	private String phone;
 	
 	public Contact() {
