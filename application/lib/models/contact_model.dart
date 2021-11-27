7f098c75c9ba5d5e79052dff182a87d14db512c1
Jonatas Caetano <jonatas.calves@gmail.com>
2021-11-27 14:13:20 -0300

    Home screen created

--- application/lib/models/contact_model.dart
+++ application/lib/models/contact_model.dart
@@ -1,3 +1,13 @@
 import 'package:scoped_model/scoped_model.dart';
 
-class ContactModel extends Model {}
+class ContactModel extends Model {
+  findAll() {}
+
+  findById() {}
+
+  post() {}
+
+  put() {}
+
+  delete() {}
+}
